f6fe8d412d929aeb1f7331813e19ed19fccff867
Albert Farhi <albert.farhi5@gmail.com>
2016-10-06 15:24:20 -0400

    add conditions for vampire answers. Tested every scenario and it works

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,33 +1,41 @@
-puts "What is your name?"
-Name = gets.chomp
+puts "what is your name?"
+name = gets.chomp
 puts "How old are you?"
-Age = gets.chomp
-Age = Age.to_i
-puts "What year were you born?"
-Dob = gets.chomp
-Dob = Dob.to_i
-Dob_age = 2016 - Dob
+age = gets.chomp
+age = age.to_i
+puts "what year were you born?"
+dob = gets.chomp
+dob = dob.to_i
+dob_age = 2016 - dob
 puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
-Wants_Garlic = gets.chomp
-case Wants_Garlic
+wants_garlic = gets.chomp
+case wants_garlic
   when "y"
-    Wants_Garlic = true
+    wants_garlic = true
   when "n"
-    Wants_Garlic = false
+    wants_garlic = false
   end
 
-puts "Would you like to enroll in the company's health insurance? (y/n)"
-Wants_health_insurance = gets.chomp
-case Wants_health_insurance
+puts "would you like to enroll in the company's health insurance? (y/n)"
+wants_health_insurance = gets.chomp
+case wants_health_insurance
   when "y"
-    Wants_health_insurance = true
+    wants_health_insurance = true
   when "n"
-    Wants_health_insurance = false
+    wants_health_insurance = false
   end
-Result = nil
-if (Dob_age == Age) && (Wants_Garlic || Wants_health_insurance)
-  Result = "Probably not a vampire."
-elsif (Dob_age != Age) && (Wants_Garlic == false || Wants_health_insurance == false)
-  Result = "Probably a vampire."
-end
-print Result
+
+result = "Results inconclusive."
+  if (dob_age == age) && (wants_garlic || wants_health_insurance)
+    result = "Probably not a vampire."
+  end
+  if (dob_age != age) && (wants_garlic == false || wants_health_insurance == false)
+    result = "Probably a vampire."
+  end
+  if (dob_age != age) && (wants_garlic == false && wants_health_insurance == false)
+    result = "Almost certainly a vampire."
+  end
+  if name == "Drake Cula" || name == "Tu Fang"
+    result = "Definitely a vampire."
+  end
+print result
