469beede777e49965b228dffa0f6c977a47540d1
Albert Farhi <albert.farhi5@gmail.com>
2016-10-13 18:48:24 -0400

    add driver code to 5.4. release 2

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,3 +1,4 @@
+# DATA STRUCTURE
 computer = {
   peripherals: [
     ['speakers', 'headphones'],
@@ -9,9 +10,8 @@ computer = {
     motherboard: {
       "Brand" => "Asus",
       series: "Z-series",
-      model_num: "Z100-A",
-      }
-    },
+      model_num: "Z100-A"
+      },
     cpu: {
       brand: 'Intel',
       gen: 'skylake',
@@ -32,5 +32,58 @@ computer = {
           }
           ]
         ],
-     }
+      }
+  }
 }
+
+
+
+
+# DRIVER CODE
+
+## PRINT THE 3RD SET OF PERIPHERALS
+puts "===================="
+puts "===================="
+puts "3rd set of peripherals"
+p computer[:peripherals][2]
+
+## PRINT THE SERIES OF THE MOTHERBOARD 
+puts "===================="
+puts "===================="
+puts "Series of motherboard"
+p computer[:components][:motherboard][:series]
+
+## PRINT THE BRAND OF THE MOTHERBOARD
+puts "===================="
+puts "===================="
+puts "Brand of MOBO:"
+p computer[:components][:motherboard]['Brand']
+
+## PRINT THE SPEED OF THE RAM
+puts "===================="
+puts "===================="
+puts "RAM Speed:"
+p computer[:components][:ram][:speed]
+
+## CHANGE THE ONBOARD GRAPHICS TO 4500
+puts "===================="
+puts "===================="
+puts "Previous onboard graphics"
+p computer[:components][:graphics][:onboard][0]
+computer[:components][:graphics][:onboard][0] = 'Intel Graphics 4500'
+puts "Updated onboard graphics"
+p computer[:components][:graphics][:onboard][0]
+
+## ADD A SECOND DEDICATED CARD WITH THESE SPECS: NVIDIA, MSI, GTX 980, 4GB
+puts "===================="
+puts "===================="
+puts "Dedicated Cards:"
+p computer[:components][:graphics][:dedicated]
+computer[:components][:graphics][:dedicated] << [card_2: {
+  brand: 'NVIDIA',
+  manufacturer: 'MSI',
+  model: 'GTX 980',
+  memory: '4GB'
+  }]
+puts "Updated List:"
+p computer[:components][:graphics][:dedicated]
