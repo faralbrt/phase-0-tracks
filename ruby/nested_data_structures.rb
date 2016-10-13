7477076080da85ebb766f2021c293a953b5fcef3
Albert Farhi <albert.farhi5@gmail.com>
2016-10-13 17:34:54 -0400

    add nested data structure. 5.4. release 2

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,36 @@
+computer = {
+  peripherals: [
+    ['speakers', 'headphones'],
+    'monitor',
+    ['mouse', 'keyboard']
+    ],
+  components: {
+    case: 'Rosewill Challenger',
+    motherboard: {
+      "Brand" => "Asus",
+      series: "Z-series",
+      model_num: "Z100-A",
+      }
+    },
+    cpu: {
+      brand: 'Intel',
+      gen: 'skylake',
+      core_series: 'i7',
+      speed: 3.4,
+      },
+    ram:{
+      brand: 'Corsair',
+      speed: '3000',
+      },
+    graphics:{
+      onboard: ['Intel Graphics 4400'],
+      dedicated: [
+        [card_1: {
+          brand: 'Nvidia',
+          manufacturer: 'EVGA',
+          model: 'GTX 770'
+          }
+          ]
+        ],
+     }
+}
