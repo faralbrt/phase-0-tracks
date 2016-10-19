34bfeeb9420bc5e12fd674887ce6b3e26fce25fe
Albert Farhi <albert.farhi5@gmail.com>
2016-10-18 21:56:13 -0400

    add design for puppy class.

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+# SPECIES ------------------------
+# Cute Puppy
+
+# CHARACTERISTICS ----------------
+# Name: Varies
+# Eye Color: Varies
+# Age: varies
+# Leg count: 4
+
+# BEHAVIOR -----------------------
+# Jump
+# Wag tail
+# Eat
+# Sleep
