7b891edf6b0dc2e55ddc5099a8bf387c30738a80
Albert Farhi <albert.farhi5@gmail.com>
2016-10-19 14:55:55 -0400

    add logic and driver code for player class.

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,4 +1,4 @@
-# LOGIC
+# LOGIC - PUPPY
 class Puppy
 
   def fetch(toy)
@@ -28,7 +28,7 @@ class Puppy
 
 end
 
-# DRIVER CODE
+# DRIVER CODE - PUPPY
 puppy1 = Puppy.new
 p puppy1
 puppy1.fetch('ball')
@@ -36,3 +36,60 @@ puppy1.speak(4)
 puppy1.roll_over
 puppy1.dog_years(35)
 puppy1.play_dead
+
+# LOGIC - PLAYERS
+
+class Player
+	def initialize
+		puts "Player is being created..."
+	end
+
+	def run_forward
+		puts "*run forward*"
+	end
+
+	def grab(object)
+		puts "#{object} grabbed."
+	end
+
+end
+
+# DRIVER CODE - PLAYERS
+list_players = []
+50.times do 
+	list_players << Player.new
+end
+
+p list_players
+
+list_players.each do |player|
+	player.run_forward
+	player.grab('sword')
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
