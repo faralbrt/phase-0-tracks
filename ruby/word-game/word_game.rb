18ec9e1c76be0a5179393d0ff2b7f645313507f1
Albert Farhi <albert.farhi5@gmail.com>
2016-10-21 11:57:41 -0400

    initialize files for guessing game

---
+++ ruby/word-game/word_game.rb
@@ -0,0 +1,26 @@
+# PLAN
+# GOAL: reveal the letters of a word or the whole word by guessing
+# create a WordGame class
+
+# allow read access to the word_guessed
+# allow access to read and change the secret_word
+# initialize the secret_word = the word to be guessed
+# initialize the secret_letters = array of letters in the word
+# initialize the guess_count = 0
+# initialize the guess_limit = length of word times 2
+# initialize the win = false
+# initialize the word_guessed = '-' multiplied by the length of the word
+
+# create a method which determines if limit of guesses has been reached
+  # if guess_count is greater than guess_limit
+    # return true
+  # Otherwise,
+    # return false
+
+# create a method that takes in a string as an argument
+  # if the string matches the secret_word
+    # set word_guessed equal to the secret_word
+  # if the string matches one of the secret_letters
+
+
+
