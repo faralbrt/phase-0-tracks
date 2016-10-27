1c489e49a04931de1e3015000c8531dafc459294
Albert Farhi <albert.farhi5@gmail.com>
2016-10-27 15:03:15 -0400

    add arrays for names and colors, also push 5th name and color

---
+++ js/data_structures.js
@@ -0,0 +1,12 @@
+var colors = ["blue", "green", "purple", "brown"];
+var names = ["Ed", "Tom", "Bobby", "Champ"];
+
+console.log("List of colors: " + colors)
+console.log("List of names: " + names)
+
+colors.push("yellow")
+names.push("Stud")
+
+console.log("\n--------------")
+console.log("List of colors: " + colors)
+console.log("List of names: " + names)
