9168498d5ef7cded3b9f4c9848bfb2183deb23ed
Albert Farhi <albert.farhi5@gmail.com>
2016-11-06 14:06:21 -0500

    create events table and add logic for inserting data

--- databases/calendar_app/main.rb
+++ databases/calendar_app/main.rb
@@ -1,6 +1,7 @@
 require 'sqlite3'
 require 'faker'
 
+# LOGIC
 db = SQLite3::Database.new("calendar.db")
 
 create_dates_cmd = <<-SQL
@@ -12,4 +13,25 @@ create_dates_cmd = <<-SQL
       )
 SQL
 
+create_events_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS events(
+    id INTEGER PRIMARY KEY,
+    title VARCHAR(255),
+    description VARCHAR(255),
+    time INT,
+    date_id INT,
+    FOREIGN KEY (date_id) REFERENCES dates(id)
+    )
+SQL
+
+def create_date(db, day, month, year)
+  db.execute("INSERT INTO dates (day, month, year) VALUES (?,?,?)" [day, month, year])
+end
+
+def create_event(db, title, description, time, date_id)
+  db.execute("INSERT INTO events (title, description, time, date_id) VALUES (?,?,?,?)" [title, description, time, date_id])
+end
+
+# DRIVER CODE
 db.execute(create_dates_cmd)
+db.execute(create_events_cmd)
