b6943abea0671b273a9b49c8f72beb41a00af046
Albert Farhi <albert.farhi5@gmail.com>
2016-11-06 13:35:39 -0500

    add database to store dates and events

--- databases/calendar_app/main.rb
+++ databases/calendar_app/main.rb
@@ -1,3 +1,15 @@
 require 'sqlite3'
 require 'faker'
 
+db = SQLite3::Database.new("calendar.db")
+
+create_dates_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS dates(
+      id INTEGER PRIMARY KEY,
+      day INT,
+      month INT,
+      year INT
+      )
+SQL
+
+db.execute(create_dates_cmd)
